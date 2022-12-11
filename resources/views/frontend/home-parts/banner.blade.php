590368c6a9d361a52ed735b5cf939775e5bc8547
gopal Sangroula <mr.gopalsangroula@gmail.com>
2022-12-11 08:20:31 +0545

    file uplad

--- resources/views/frontend/home-parts/banner.blade.php
+++ resources/views/frontend/home-parts/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
         <div class="card text-center">
             <div class="card-body">
-                <h1 class="ah-title"> Gopalसरकारी निकायमा गृह र रक्षा सबैभन्दा धनी, अन्यको सम्पत्ति कति ?</h1>
+                <h1 class="ah-title"> Gopal sangroula सरकारी निकायमा गृह र रक्षा सबैभन्दा धनी, अन्यको सम्पत्ति कति ?</h1>
                 <img src="https://www.onlinekhabar.com/wp-content/uploads/2022/11/Surkhet-buspark-1230-px-1024x576.png" class="card-img-top" alt="...">
               <p class="fs-5 mt-2">अहिले आएको परिणाममा सत्ता गठबन्धन जे कामका लागि निर्माण भएको थियो, त्यसमा इमानदार भएर बस्ने हो भने संविधानको रक्षा र स्थिरताका लागि समस्या भएन । किनकि गठबन्धनसँग केन्द्रमा सरकार निर्माणका लागि चाहिने म्याजिक नम्बर १३८ छ ।</p>
             </div>
